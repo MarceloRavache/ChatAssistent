10d7d1792c992921c04eb003058c31af895333d8
JMRavache <jm.ravache@insightlab.ufc.br>
2020-11-24 02:14:31 -0300

    [UPDATE] ações externas adicionadas

--- src/components/ChatAssistent/ChatAssistent.tsx
+++ src/components/ChatAssistent/ChatAssistent.tsx
@@ -1,16 +1,36 @@
-import React,{ Component } from 'react';
-import { title } from 'process';
+import React,{useState} from 'react';
 
-type ChatAssitentProps = {
-    title: string
+import './styles.css'
+interface Props {
+    readonly title: string
 }
 
-export default class ChatAssitent extends Component<ChatAssitentProps>{
-    render(){
-        return(
-            <div>
-                {title}
-            </div>
-        )
+const ChatAssitent: React.FC<Props> = ({title}) => {
+    const [visibleChat,setVisibleChat] = useState(false);
+    const [chatInput,setChatInput] = useState("");
+
+    const handleVisibleChat = () => {
+        if(visibleChat){
+            const chatContent = document.getElementsByClassName("chat__content")  as HTMLCollectionOf<HTMLElement>;
+            chatContent[0].style.display = 'none' ;
+            setVisibleChat(false);
+        }else{
+            const chatContent = document.getElementsByClassName("chat__content")  as HTMLCollectionOf<HTMLElement>;
+            chatContent[0].style.display = 'block' ;
+            setVisibleChat(true);
+        }
+        
     }
+    return(
+        <>
+        <div className="chat">
+            <button className="chat__bar" onClick={()=>handleVisibleChat()}>{title}</button>
+            <div className="chat__content" id="chat__content">
+                <div className="chat_history"></div>
+                <input className="chat_input" type="text" value={chatInput} onChange={e=>setChatInput(e.target.value)}/>
+            </div>
+        </div>
+        </>
+    )
 }
+export default ChatAssitent;
